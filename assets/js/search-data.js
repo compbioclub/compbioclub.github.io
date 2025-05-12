d029d086cc2aed370b13d302cff12087f6f7a913
lllvcs <22395524+lllvcs@users.noreply.github.com>
2025-05-12 17:47:36 +0000

    Deploying to gh-pages from @ compbioclub/compbioclub.github.io@5775bc358467864a78cf2e11924fe9499f555e50 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-people",
+          title: "People",
+          description: "Talented Lab Members of CompBioClub",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "nav-hiring",
           title: "Hiring",
           description: "",
